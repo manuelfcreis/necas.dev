723a0ed27d33ba74ab855ed1d1d112fec3c2752b
Manuel Costa Reis <manuelfcreis@gmail.com>
2020-02-01 12:48:38 +0000

    adapt eye and icons

--- src/components/shared/eye.tsx
+++ src/components/shared/eye.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import '../../stylesheets/shared/eye.scss';
-import classnames from 'classnames';
+import React, { Component } from "react";
+import "../../stylesheets/shared/eye.scss";
+import classnames from "classnames";
 
 interface Props {
   small: boolean;
@@ -12,30 +12,56 @@ interface Props {
 }
 
 class Eye extends Component<Props> {
+  eyeRef: React.RefObject<HTMLDivElement>;
+
   constructor(props: Props) {
     super(props);
+
+    this.eyeRef = React.createRef();
+  }
+
+  calculateEyePosition(mouse: number, window: number, kind: any): string {
+    let eyePosition = 0;
+
+    if (this.eyeRef.current) {
+      eyePosition =
+        kind === "top"
+          ? this.eyeRef.current.getBoundingClientRect().top +
+            this.eyeRef.current.getBoundingClientRect().height / 2
+          : this.eyeRef.current.getBoundingClientRect().left +
+            this.eyeRef.current.getBoundingClientRect().width / 2;
+    }
+
+    const mouseRelative = mouse / window;
+    const eyeRelative = eyePosition / window;
+    const position = mouseRelative / (eyeRelative * 2);
+
+    return String(Math.min(Math.max(Math.round(position * 100), 20), 80)) + "%";
   }
 
   render(): JSX.Element {
-    const topValue =
-      String(Math.round((this.props.positions.y / window.innerHeight) * 100)) +
-      '%';
-    const leftValue =
-      String(Math.round((this.props.positions.x / window.innerWidth) * 100)) +
-      '%';
+    const topValue = this.calculateEyePosition(
+      this.props.positions.y,
+      window.innerHeight,
+      "top"
+    );
+
+    const leftValue = this.calculateEyePosition(
+      this.props.positions.x,
+      window.innerWidth,
+      "left"
+    );
 
     return (
       <div
+        ref={this.eyeRef}
         className={classnames(
-          'eye',
-          this.props.small ? 'smallEye' : '',
-          this.props.hide ? 'invisible' : ''
+          "eye",
+          this.props.small ? "smallEye" : "",
+          this.props.hide ? "invisible" : ""
         )}
       >
-        <div
-          className="ball"
-          style={this.props.small ? {} : { top: topValue, left: leftValue }}
-        >
+        <div className="ball" style={{ top: topValue, left: leftValue }}>
           <div className="iris" />
         </div>
       </div>
