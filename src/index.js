93303c734c065ab2a6a9f1e80cd3dba7fa496fcb
Manuel Costa Reis <manuelfcreis@gmail.com>
2020-02-01 16:59:15 +0000

    rename app file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheets/index.scss";
-import App from "./components/app.tsx";
+import App from "./components/App.tsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/tests/components/app.test.js
+++ src/tests/components/app.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "../../components/app";
+import App from "../../components/App";
 import { shallow } from "enzyme";
 
 it("renders correctly", () => {
